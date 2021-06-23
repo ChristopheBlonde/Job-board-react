846095eedde5ace9c874af9739ec91a62190b3bd
Christophe Blonde <warlutin@mbpdechristophe.home>
2021-06-23 22:42:19 +0200

    responsive-start

--- src/conponents/Footer.js
+++ src/conponents/Footer.js
@@ -1,6 +1,14 @@
 const Footer = (props) => {
   return (
     <footer className={props.className}>
+      <a
+        className="linkIcon"
+        href="https://apollo.lereacteur.io/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <img src={props.icon} alt="" />{" "}
+      </a>
       <p className="link">
         Made with <span>React</span> at &nbsp;
         <a
@@ -19,6 +27,14 @@ const Footer = (props) => {
           <img src={props.image} alt="" /> Chris
         </a>
       </p>
+      <a
+        className="linkIcon"
+        href="https://github.com/ChristopheBlonde"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <img src={props.image} alt="" />
+      </a>
     </footer>
   );
 };
